d128a0195b7dddc476ae16d8c8722339a2eaf6e0
Ansari dilshad <50269759+Ansaridilshad@users.noreply.github.com>
2024-08-13 20:57:29 +0000

    container config done

---
+++ js/variable01.js
@@ -0,0 +1,15 @@
+const   accountId = 2525285 ;
+let  accountEmail = "Dilshad@gmail.com" ;
+var   accountPassword  = "12345" ;
+accountCity = " Mumbai ";
+let accountState ;
+
+
+// accountId = "455844"
+accountEmail = "priya@gmail.com" 
+accountPassword = "00000"
+accountCity = "delhi "
+
+console.table([accountEmail,accountId,accountPassword,accountCity,accountState])
+// accountId = 45114 ;
+
