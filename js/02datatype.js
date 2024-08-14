c38c875e8057b7116c97a9124ff7ad0a620ab334
Ansari dilshad <50269759+Ansaridilshad@users.noreply.github.com>
2024-08-14 11:27:55 +0000

    converstion data tpye

---
+++ js/02datatype.js
@@ -0,0 +1,21 @@
+
+// data type in js
+// 1. Number
+// 2. String
+// 3. Boolean
+// 4. Undefined
+// 5. Null
+// 6. Symbol
+// 7. BigInt
+// 8. Object (includes Arrays, Functions, etc.)
+
+
+let  score = "33" ;
+// console.log(typeof(score))
+let valueInnumber = Number(score)
+// console.log(typeof(valueInnumber))
+let check = 4535353 ;
+let concvert = Boolean( check)
+console.log(concvert)
+console.log(typeof concvert)
+
