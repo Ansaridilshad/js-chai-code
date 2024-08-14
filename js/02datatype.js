286a3a42676e4ef13ac8b21e3ad650a83e724ef6
Ansari dilshad <50269759+Ansaridilshad@users.noreply.github.com>
2024-08-14 12:56:07 +0000

    data tpye

--- js/02datatype.js
+++ js/02datatype.js
@@ -10,12 +10,50 @@
 // 8. Object (includes Arrays, Functions, etc.)
 
 
-let  score = "33" ;
-// console.log(typeof(score))
-let valueInnumber = Number(score)
-// console.log(typeof(valueInnumber))
-let check = 4535353 ;
-let concvert = Boolean( check)
-console.log(concvert)
-console.log(typeof concvert)
+// let  score = "33" ;
+// // console.log(typeof(score))
+// let valueInnumber = Number(score)
+// // console.log(typeof(valueInnumber))
+// let check = 4535353 ;
+// let concvert = Boolean( check)
+// console.log(concvert)
+// console.log(typeof concvert)
 
+////// OPREATION //////////
+
+
+// console.log(1 + "2");
+// console.log('1' + 2);
+// console.log("1" + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+
+
+//// primitive data type //////
+
+//  7  Type :  string , number , boolean , symbol , bigInt, null , undefind ,
+
+let str  = "string"
+let Number  =  123;
+let boolean  = true  ;
+let id= Symbol("12345") ;
+let bigInt =  465443464674347134464141n ;
+let undefind ;
+let Null =  null ;
+// console.table([str,Number,boolean,id,bigInt,undefind,null])
+
+ // Refrence  (non Primitive )
+ // Array  , object  , funtion 
+ let array  =  [1,2,3,55,4,56,7] ;
+
+ 
+//  let obj = {
+//     car : "red",
+//     brand : "tata",
+//     nation  : "INDIA"
+    
+//   }
+
+
+///////Stack momery = (Primitive type)     and  Heap Memory = (NON-Primitive type) 
